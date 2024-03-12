4cd2840931585070248e64453663ebac3c9ef48c
Rohit Pathak <rohitpathak88@gmail.com>
2024-03-12 12:33:24 +0530

    Side Bar added

--- first-project/src/App.js
+++ first-project/src/App.js
@@ -1,13 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-import { Auth } from './Components/Authentication/Auth';
+import './App.scss';
+import 'boxicons/css/boxicons.min.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import AppLayout from './components/layout/AppLayout';
+import Blank from './pages/Blank';
 
 function App() {
-  return (
-    <div className="App">
-      <Auth/>
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<AppLayout />}>
+                    <Route index element={<Blank />} />
+                    <Route path='/started' element={<Blank />} />
+                    <Route path='/calendar' element={<Blank />} />
+                    <Route path='/user' element={<Blank />} />
+                    <Route path='/order' element={<Blank />} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
